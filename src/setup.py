53b10b79148acc7fe1fb12b609c5a8eab69e3d96
Jared Coleman <jaredraycoleman@gmail.com>
2024-10-05 08:16:39 -0700

    Updates package dependencies

--- src/setup.py
+++ src/setup.py
@@ -4,5 +4,14 @@ setup(
     name='yaduha',
     version='0.1',
     packages=['yaduha'],
-    install_requires=[]
+    install_requires=[
+        'numpy>=2.0.0',
+        'pandas==2.0.0',
+        'python-dotenv==1.0.1',
+        'openai==1.51.0',
+        'spacy==3.8.2',
+    ],
+    dependency_links=[
+        'git+ssh://git@github.com/kubishi/rbo.git#egg=rbo',
+    ]
 )
